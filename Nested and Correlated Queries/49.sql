acd0ff7a64e59acd97628e8645d131057f1348cf
kotaasha2004 <168574635+kotaasha2004@users.noreply.github.com>
2025-06-28 00:00:08 +0530

    Update 49.sql

--- Nested and Correlated Queries/49.sql
+++ Nested and Correlated Queries/49.sql
@@ -1,1 +1,7 @@
+SELECT d.name AS department_name, COUNT(e.emp_id) AS total_employees
+FROM Department d
+JOIN Employee e ON d.department_id = e.department_id
+GROUP BY d.department_id, d.name
+ORDER BY total_employees DESC
+LIMIT 1;
 
